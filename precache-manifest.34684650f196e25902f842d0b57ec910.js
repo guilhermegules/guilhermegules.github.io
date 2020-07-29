f571a81aa47d1543b1bd6fd6847f1dbd5210e57c
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-07-28 22:24:30 -0300

    feat: adding builded project

---
+++ precache-manifest.34684650f196e25902f842d0b57ec910.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d012272806abb4d8b4f7c3580e0b51f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1044b4d1eef27fdd2891",
+    "url": "/static/js/2.8089b69e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8089b69e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26aa588c701e5de42929",
+    "url": "/static/js/main.703cef81.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "e42b15d09cd1c1ef401bf1332aec3381",
+    "url": "/static/media/angular.e42b15d0.svg"
+  },
+  {
+    "revision": "7866fa098743c3ca0c6b4b5c74e2bd4b",
+    "url": "/static/media/background.7866fa09.jpg"
+  },
+  {
+    "revision": "e0ebd79881d42b75efc57a1b09f73fef",
+    "url": "/static/media/javascript.e0ebd798.svg"
+  },
+  {
+    "revision": "f90b0ffdc193ce323211d2c9eaccc745",
+    "url": "/static/media/me.f90b0ffd.jpg"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/static/media/nodejs.b337a2fb.svg"
+  },
+  {
+    "revision": "9a63b1ac782e1a563ce6106571d626f8",
+    "url": "/static/media/react-native.9a63b1ac.svg"
+  },
+  {
+    "revision": "6b592eacd6b777208c932ae22baa5043",
+    "url": "/static/media/react.6b592eac.svg"
+  },
+  {
+    "revision": "42daaa9db5619dd640cc612c16216e43",
+    "url": "/static/media/typescript.42daaa9d.svg"
+  },
+  {
+    "revision": "7943559e94e46cfb4c1eba6224038e9c",
+    "url": "/static/media/vue-js.7943559e.svg"
+  },
+  {
+    "revision": "837b970d5c77507b2ea4bb0ec18059a0",
+    "url": "/static/media/webpack.837b970d.svg"
+  }
+]);
